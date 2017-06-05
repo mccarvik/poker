9434bcb7d50abdd5065e74a117a20d04e37cbc22
mccarvik <mccarviks@gmail.com>
2017-06-05 17:59:58 +0000

    making progress getting everything set up

---
+++ app/deck_cards/deck.py
@@ -0,0 +1,34 @@
+from app.deck_cards.card import Card
+
+class Deck():
+    ''' Class to represent a deck of cards '''
+    
+    SUITS = ['s', 'h', 'c', 'd']
+    VALUES = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
+    VAL_MAP = {
+        'A' : 14,
+        'K' : 13,
+        'Q' : 12,
+        'J' : 11,
+        '10' : 10,
+        '9' : 9,
+        '8' : 8,
+        '7' : 7,
+        '6' : 6,
+        '5' : 5,
+        '4' : 4,
+        '3' : 3,
+        '2' : 2
+    }
+    
+    def __init__(self):
+        self.initialize()
+    
+    def initialize(self):
+        self._cards = []
+        for s in self.SUITS:
+            for v in self.VALUES:
+                self._cards.append(Card(v,s))
+    
+    def drawRandom(self):
+        pass
---
+++ app/scripts/nav_bar.js
@@ -0,0 +1,48 @@
+var app = angular.module("poker", [])
+  .config(function ($routeProvider, $locationProvider, $httpProvider) {
+
+    $routeProvider.when('/single_hand',
+    {
+      templateUrl:    'single_hand.html',
+      controller:     'SingleHandCtrl'
+    });
+    $routeProvider.when('/multi_hand',
+    {
+      templateUrl:    'multi_hand.html',
+      controller:     'MultiHandCtrl'
+    });
+    $routeProvider.when('/play_live',
+    {
+      templateUrl:    'play_live.html',
+      controller:     'PlayLiveCtrl'
+    });
+    $routeProvider.otherwise(
+    {
+      redirectTo:     '/home.html',
+      controller:     'NavCtrl', 
+    }
+  );
+});
+
+app.controller('NavCtrl', 
+['$scope', '$location', function ($scope, $location) {  
+  $scope.navClass = function (page) {
+    var currentRoute = $location.path().substring(1) || 'home';
+    return page === currentRoute ? 'active' : '';
+  };
+}]);
+
+app.controller('SingleHandCtrl', function($scope, $compile) {
+  console.log('inside single hand controller');
+
+});
+
+app.controller('MultiHandCtrl', function($scope, $compile) {
+  console.log('inside multi hand controller');
+
+});
+
+app.controller('PlayLiveCtrl', function($scope, $compile) {
+  console.log('inside play live controller');
+
+});
