0f50237fb39e68804a83b1fd7e997f63c59467cb
mccarvik <mccarviks@gmail.com>
2017-07-28 12:21:42 +0000

    fixed high card bug

--- app/handlers/run_multi_handler.py
+++ app/handlers/run_multi_handler.py
@@ -24,8 +24,8 @@ def cleanInputs(args):
     return (hands, board)
 
 if __name__ == "__main__":
-    # args = read_in()
-    args = {"holecard1":{"suit":"c","val":"3"},"holecard2":{"suit":"c","val":"J"},"board1":{"suit":"s","val":"6"},"board2":{"suit":"h","val":"4"},"board3":{"suit":"d","val":"6"},"saved0_1":{"suit":"h","val":"A"},"saved0_2":{"suit":"h","val":"9"}}
+    args = read_in()
+    # args = {"holecard1":{"suit":"c","val":"3"},"holecard2":{"suit":"c","val":"J"},"board1":{"suit":"s","val":"6"},"board2":{"suit":"h","val":"4"},"board3":{"suit":"d","val":"6"},"saved0_1":{"suit":"h","val":"A"},"saved0_2":{"suit":"h","val":"9"}}
     # args = {"holecard1":{"suit":"c","val":"3"},"holecard2":{"suit":"c","val":"J"},"saved0_1":{"suit":"h","val":"A"},"saved0_2":{"suit":"h","val":"9"}}
     hands, board = cleanInputs(args)
     # print(hands, board)
